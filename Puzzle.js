90e0e6c1244acf1fa94c72d35f98f5fce7cf6707
aleksandar-stotka <aangelevski6@gmail.com>
2025-08-12 09:38:46 +0200

    test

--- Puzzle.js
+++ Puzzle.js
@@ -1167,25 +1167,10 @@
       } // loadFile
     } //  // scope for loadFile
 
- 
-  function loadInitialFile(imageUrl) {
-    puzzle.srcImage.src = imageUrl;
-  }
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const imageGrid = document.getElementById("imageGrid");
-    const images = imageGrid.querySelectorAll("img");
-
-    images.forEach(img => {
-      img.addEventListener("click", () => {
-        // Load clicked image into puzzle
-        loadInitialFile(img.src);
-
-        // Hide the entire image grid
-        imageGrid.style.display = "none";
-      });
-    });
-  });
+    function loadInitialFile() {
+      puzzle.srcImage.src = "https://images.sbs.com.au/drupal/yourlanguage/public/bibi4-rez.jpg ";
+    }
+    
 
     //-----------------------------------------------------------------------------
     function imageLoaded(puzzle) {
